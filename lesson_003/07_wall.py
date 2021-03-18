1010b3bad442e80571a3c4164f277599bd305228
Альфир Тякин <alfir7777@gmail.com>
2021-03-18 22:09:33 +0500

    lesson_003:07 fixed

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -9,33 +9,26 @@ import simple_draw as sd
 # Использовать вложенные циклы for
 
 
-# TODO оказывается еще нужно смещение
-# FIXME еще в есть функция вывода прямоугольника
-# sd.resolution = (600, 600)
+sd.resolution = (600, 600)
 x = 100
 y = 50
 
 step_y = 0
+
 for _ in range(0, 601, y):
     step_x = 0
     if (step_y / 10) % 2:
         for _ in range(0, 601, x):
             point_x = sd.get_point(step_x, step_y)
-            point_y = sd.get_point(step_x, y+step_y)
-            sd.line(point_x, point_y, color=sd.COLOR_ORANGE, width=5)
-            point_x1 = sd.get_point(step_y, y+step_y)
-            point_y1 = sd.get_point(step_x, y+step_y)
-            sd.line(point_x1, point_y, color=sd.COLOR_ORANGE, width=5)
+            point_y = sd.get_point(x+step_x, y+step_y)
+            sd.rectangle(point_x, point_y, color=sd.COLOR_ORANGE, width=3)
             step_x += x
             time.sleep(0.01)
     else:
         for _ in range(0, 601, x):
-            point_x = sd.get_point(step_x+50, step_y)
-            point_y = sd.get_point(step_x+50, y+step_y)
-            sd.line(point_x, point_y, color=sd.COLOR_ORANGE, width=5)
-            point_x1 = sd.get_point(step_y, y+step_y)
-            point_y1 = sd.get_point(step_x+50, y+step_y)
-            sd.line(point_x1, point_y, color=sd.COLOR_ORANGE, width=5)
+            point_x = sd.get_point(step_x+y, step_y)
+            point_y = sd.get_point(x+step_x+y, y+step_y)
+            sd.rectangle(point_x, point_y, color=sd.COLOR_ORANGE, width=3)
             step_x += x
             time.sleep(0.01)
     step_y += y
