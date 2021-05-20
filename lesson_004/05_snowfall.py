310e0ee609acee67ec93efc34b9ae4db058ff9c3
Alfir Tyakin <alfir777@rambler.ru>
2021-05-20 13:25:54 +0500

    executed lesson_004

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -18,24 +18,44 @@ N = 20
 
 sd.resolution = (1200, 600)
 
-x = []
-for _ in range(N):
-    x.append(sd.random_number(50, 1150))
-y = []
-for _ in range(N):
-    y.append(sd.random_number(100, 550))
+y = 550
+
+snowflakes = {}
+
+for i in range(N):
+    snowflakes[i] = {'length': sd.random_number(5,15),
+                     'x': sd.random_number(0, sd.resolution[0]),
+                     'y': y,
+                     'factor_a': sd.random_number(1, 10)/10,
+                     'factor_b': sd.random_number(1, 10)/10,
+                     'factor_c': sd.random_number(10, 120)}
 
 while True:
     sd.start_drawing()
-    for i in range(len(x)):
-        if i > 0:
-            point1 = sd.get_point(x[i-1], y[i-1])
-            sd.snowflake(center=point1, length=50, color=sd.COLOR_WHITE)
-        point = sd.get_point(x[i], y[i])
-        if y[i] > 50:
-            sd.snowflake(center=point, length=50, color=sd.background_color)
-            y[i] -= sd.random_number(-1, 15)
-            x[i] = x[i] + sd.random_number(-15, 15)
+    for snowflakes_k, snowflakes_v in snowflakes.items():
+        start_point = sd.get_point(snowflakes_v['x'], snowflakes_v['y'])
+        sd.snowflake(center=start_point,
+                     length=snowflakes_v['length'],
+                     color=sd.background_color,
+                     factor_a=snowflakes_v['factor_a'],
+                     factor_b=snowflakes_v['factor_b'],
+                     factor_c=snowflakes_v['factor_c'])
+
+        snowflakes_v['x'] += sd.random_number(-5, 5)
+        snowflakes_v['y'] -= snowflakes_v['length']
+
+        next_point = sd.get_point(snowflakes_v['x'], snowflakes_v['y'])
+        sd.snowflake(center=next_point,
+                     length=snowflakes_v['length'],
+                     color=sd.COLOR_WHITE,
+                     factor_a=snowflakes_v['factor_a'],
+                     factor_b=snowflakes_v['factor_b'],
+                     factor_c=snowflakes_v['factor_c'])
+
+        if snowflakes_v['y'] < 50:
+            snowflakes_v['y'] = y
+            snowflakes_v['x'] = sd.random_number(0, sd.resolution[0])
+
     sd.finish_drawing()
     sd.sleep(0.1)
     if sd.user_want_exit():
