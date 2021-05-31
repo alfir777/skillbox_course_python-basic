11beed789efb91f144aade51625c2f80e92fb049
Alfir Tyakin <alfir777@rambler.ru>
2021-05-31 14:49:25 +0500

    executed lesson_008:01:02

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -21,7 +21,40 @@
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+
+class Statistics_by_letter:
+    count_letter = 0
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.all_letters = set()
+        self.all_letters_count = {}
+
+    def collect(self):
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                for i in line:
+                    if i.isalpha():
+                        self.all_letters.add(i)
+                        if i in self.all_letters_count:
+                            self.all_letters_count[i] += 1
+                            self.count_letter += 1
+                        else:
+                            self.all_letters_count[i] = 0
+                            self.all_letters_count[i] += 1
+                            self.count_letter += 1
+
+
+statistics_letters = Statistics_by_letter(file_name='voyna-i-mir.txt')
+statistics_letters.collect()
+print('+---------+----------+')
+print('|  буква  | частота  |')
+print('+---------+----------+')
+for key, value in statistics_letters.all_letters_count.items():
+    print("|{key:^9}| {value:^9}|".format(key=key, value=value))
+print('+---------+----------+')
+print('|  итого  | {count:^9}|'.format(count=statistics_letters.count_letter))
+print('+---------+----------+')
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
