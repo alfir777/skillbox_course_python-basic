bb635c26703016ad3da106731f10fee85fa391bf
Alfir Tyakin <alfir777@rambler.ru>
2021-06-29 18:20:39 +0500

    executed lesson_011:04

--- lesson_011/04_error_log_decorator.py
+++ lesson_011/04_error_log_decorator.py
@@ -8,25 +8,51 @@
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
 
 
-def log_errors(func):
+def log_errors_v1(func):
     def surrogate(*args, **kwargs):
         file = open(file='function_errors.log', mode='a', encoding='utf-8')
         try:
             res = func(*args, **kwargs)
             return res
         except Exception as exc:
-            file.write(f'Error: {func.__name__:<10} {exc}\n')
+            param = []
+            param.extend([arg for arg in args])
+            param.extend([f'{key}={value}' for key, value in kwargs.items()])
+            file.write(f'{func.__name__:<10}'
+                       f'{param.__str__():<40}'
+                       f'{exc.__class__.__name__:<20}'
+                       f'{str(exc):<10}\n')
         file.close()
     return surrogate
 
 
+def log_errors_v2(filename=None):
+    def log_errors(func):
+        def surrogate(*args, **kwargs):
+            file = open(file=filename, mode='a', encoding='utf-8')
+            try:
+                res = func(*args, **kwargs)
+                return res
+            except Exception as exc:
+                param = []
+                param.extend([arg for arg in args])
+                param.extend([f'{key}={value}' for key, value in kwargs.items()])
+                file.write(f'{func.__name__:<10}'
+                           f'{param.__str__():<40}'
+                           f'{exc.__class__.__name__:<20}'
+                           f'{str(exc):<10}\n')
+            file.close()
+        return surrogate
+    return log_errors
+
+
 # Проверить работу на следующих функциях
-@log_errors
+@log_errors_v2(filename='function_errors.log')
 def perky(param):
     return param / 0
 
 
-@log_errors
+@log_errors_v2(filename='function_errors.log')
 def check_line(line):
     name, email, age = line.split(' ')
     if not name.isalpha():
