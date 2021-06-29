e48706949942d2623d71395127b0b1c6bc463ae2
Alfir Tyakin <alfir777@rambler.ru>
2021-06-29 15:43:23 +0500

    executed lesson_011:03

--- lesson_011/03_log_parser.py
+++ lesson_011/03_log_parser.py
@@ -14,4 +14,26 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+
+def log_parser(file_name=None):
+    event_count_nok = 0
+    previous_line = ''
+    with open(file_name, 'r', encoding='cp1251') as file:
+        for line in file:
+            line_left = line[1:17]
+            line_right = line[-4:-1]
+            if previous_line == '':
+                previous_line = line_left
+            if previous_line != line_left:
+                group_time = previous_line
+                event_count = event_count_nok
+                event_count_nok = 0
+                previous_line = line_left
+                yield group_time, event_count
+            if line_right == 'NOK':
+                event_count_nok += 1
+
+
+grouped_events = log_parser(file_name='events.txt')
+for group_time, event_count in grouped_events:
+    print(f'[{group_time}] {event_count}')
